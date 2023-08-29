c16066bc40b212e0c457426b228ce0bc36b6434a
Mohammed Jameeluddin <jameel3334@gmail.com>
2023-08-29 18:30:13 -0400

    Basic Username Text validation

--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -14,6 +14,10 @@ class SearchVC: UIViewController {
     let userTextField = GFTextFields()
     let callToActionButton = GFButtons(backgroundColor: .systemGreen, title: "Get Followers")
     
+    var isUsernameEntered: Bool {
+        return !userTextField.text!.isEmpty
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
@@ -34,6 +38,9 @@ class SearchVC: UIViewController {
     }
     
     @objc func pushFavoriteListVC() {
+        guard isUsernameEntered else {
+            return
+        }
         let followerList = FollowerListVC()
         followerList.username = userTextField.text
         followerList.title = userTextField.text
