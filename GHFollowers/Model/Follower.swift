3c91085579c1730741d0e4a6f33eee288fe26f67
Mohammed Jameeluddin <jameel3334@gmail.com>
2023-09-02 08:48:03 -0400

    regroup and model creation

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Mohammed Jameeluddin on 9/2/23.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
