ca65d7137baafe23390f118d2de6b3b560f1b225
Mohammed Jameeluddin <jameel3334@gmail.com>
2023-09-03 16:35:10 -0400

    customThreeColumnFlowLayout

--- GHFollowers/ViewControllers/FollowerListVC.swift
+++ GHFollowers/ViewControllers/FollowerListVC.swift
@@ -45,5 +45,17 @@ class FollowerListVC: UIViewController {
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
     
+    func customThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let itemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (itemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 35)
+        return flowLayout
+    }
     
 }
